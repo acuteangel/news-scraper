fa7955cce4611fc6942c116d64985fc0b241d259
AKA <acuteangel@users.noreply.github.com>
2019-01-18 03:38:53 -0800

    updated mongodlab specs

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 
 app.get("/", function(req, res) {
